743aeb962ada1c77772e371acbb55da0bcce4564
kvnjng <kvvnjng@gmail.com>
2019-01-11 15:41:08 -0500

    change position to corrected coordinate whether or not if indel or not

--- rsjson_mongo_filter.py
+++ rsjson_mongo_filter.py
@@ -1,3 +1,4 @@
+import sys
 import json
 import gzip
 import os
@@ -23,20 +24,6 @@ def getChromosome(f_in):
     return f_in.name.split('.')[0].split('-')[1][3:]
 
 
-# find GRCh37 genomic position
-def getPosition(primary_refsnp):
-    position = ''
-    for i in primary_refsnp['primary_snapshot_data']['placements_with_allele']:
-        if len(i['placement_annot']['seq_id_traits_by_assembly']) > 0:
-            assembly = i['placement_annot']['seq_id_traits_by_assembly'][0]['assembly_name']
-            is_chrom = i['placement_annot']['seq_id_traits_by_assembly'][0]['is_chromosome']
-            pos = i['alleles'][0]['allele']['spdi']['position']
-            # only choose bp from GRCh37.p13
-            if is_chrom == True and assembly == "GRCh37.p13":
-                position = str(pos)
-    return position
-
-
 # find sequence change annotations
 def getAnnotations(primary_refsnp):
     annotations = []
@@ -60,6 +47,23 @@ def getVariantType(primary_refsnp):
     return variant_type
 
 
+# find GRCh37 genomic position
+def getPosition(primary_refsnp, variant_type):
+    position = ''
+    for i in primary_refsnp['primary_snapshot_data']['placements_with_allele']:
+        if len(i['placement_annot']['seq_id_traits_by_assembly']) > 0:
+            assembly = i['placement_annot']['seq_id_traits_by_assembly'][0]['assembly_name']
+            is_chrom = i['placement_annot']['seq_id_traits_by_assembly'][0]['is_chromosome']
+            pos = i['alleles'][0]['allele']['spdi']['position']
+            # only choose bp from GRCh37.p13
+            if is_chrom == True and assembly == "GRCh37.p13":
+                if variant_type is "delins":
+                    position = str(pos)
+                else:
+                    position = str(int(pos) + 1)
+    return position
+
+
 # write output from parsing json files
 def createRecord(rsids, chromosome, position, annotations, variant_type):
     if len(rsids) > 0:
@@ -74,6 +78,7 @@ def createRecord(rsids, chromosome, position, annotations, variant_type):
             else:
                 pass
 
+
 def writeJSON(rsid, chromosome, position, annotations, variant_type):
     record = {
         "id": rsid,
@@ -87,72 +92,28 @@ def writeJSON(rsid, chromosome, position, annotations, variant_type):
         outfile.write('\n')
 
 
-# def insertMongoDB(rsid, chromosome, position, annotations, variant_type):
-#     client = MongoClient('localhost', 27017)
-#     db = client["LDLink"]
-#     record = {
-#         "id": rsid,
-#         "chromosome": chromosome,
-#         "position": position,
-#         "function": annotations,
-#         "type": variant_type
-#     }
-#     dbsnp = db.dbsnp151
-#     dbsnp.insert_one(record).inserted_id
-
-# def createIndexes():
-#     client = MongoClient('localhost', 27017)
-#     db = client["LDLink"]
-#     print "Drop indexes on ID and Genomic Coordinates if exist."
-#     try:
-#         db.dbsnp151.drop_index( "id_1" )
-#         db.dbsnp151.drop_index( "chromosome_1_position_1" )
-#     except:
-#         pass
-#     print "Indexes dropped."
-#     print "Create index on RSID."
-#     db.dbsnp151.create_index('id')
-#     print "RSID indexing completed."
-#     print "Create index on genomic coordinates"
-#     db.dbsnp151.create_index([("chromosome", ASCENDING), ("position", ASCENDING)])
-#     print "Genomic coordinates indexing completed."
-
-
 def main():
     print "Start creating json file(s) with filtered records. db: LDLink, collection: dbsnp151"
-    # iterate through each file in directory
+    filename = sys.argv[1]
     input_dir = 'json_refsnp/'
-    for filename in os.listdir(input_dir):
-        if "refsnp" in filename:
-            print filename
-            with gzip.open(input_dir + filename, 'rb') as f_in:
+    with gzip.open(input_dir + filename, 'rb') as f_in:
+        # limit lines read per file
+        # cnt = 0
+        for line in f_in:
+            rs_obj = json.loads(line.decode('utf-8'))
+            if 'primary_snapshot_data' in rs_obj:
+                rsids = getRSIDs(rs_obj)
+                chromosome = getChromosome(f_in)
+                annotations = getAnnotations(rs_obj['primary_snapshot_data'])
+                variant_type = getVariantType(rs_obj)
+                position = getPosition(rs_obj, variant_type)
+                createRecord(rsids, chromosome, position, annotations, variant_type)
                 # limit lines read per file
-                # cnt = 0
-                for line in f_in:
-                    # try:
-                    rs_obj = json.loads(line.decode('utf-8'))
-                    # last_line = json.dumps(rs_obj)
-                    if 'primary_snapshot_data' in rs_obj:
-                        rsids = getRSIDs(rs_obj)
-                        # print rsids
-                        chromosome = getChromosome(f_in)
-                        position = getPosition(rs_obj)
-                        annotations = getAnnotations(rs_obj['primary_snapshot_data'])
-                        variant_type = getVariantType(rs_obj)
-                        # create and insert row into sqlite database
-                        createRecord(rsids, chromosome, position, annotations, variant_type)
-                        # limit lines read per file
-                        # cnt = cnt + 1
-                        # if (cnt > 50000):
-                        #     break
+                # cnt = cnt + 1
+                # if (cnt > 50000):
+                #     break
     print "JSON file(s) completed."
 
-    # print "Start indexing MongoDB."
-    # createIndexes()
-    # print "Indexing completed."
-
-
-    # print script's run time when finshed
     print("--- %s seconds ---" % (time.time() - start_time))
 
 
