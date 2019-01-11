65ee65b201b59fae58ab9aaac2a172869915e7a1
kvnjng <kvvnjng@gmail.com>
2019-01-11 15:57:08 -0500

    fix batch script

--- rsjson_run.sh
+++ rsjson_run.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-sbatch --time=48:00:00 --mail-type=ALL,TIME_LIMIT_80 python rsjson_mongo_filter.py refsnp-chr1.json.gz
-sbatch --time=48:00:00 --mail-type=ALL,TIME_LIMIT_80 python rsjson_mongo_filter.py refsnp-chr2.json.gz
+sbatch --time=48:00:00 --mail-type=ALL,TIME_LIMIT_80 rsjson_chr_1.sh
+sbatch --time=48:00:00 --mail-type=ALL,TIME_LIMIT_80 rsjson_chr_2.sh
